92dc20cde21648af149f75167c6c14ef5e984e3c
priya modak <priyamodak5464@gmail.com>
2020-12-23 02:27:57 -0500

    Project Uploaded

---
+++ WebApplication9/Controllers/EmployeeController.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using WebApplication9.Context;
+
+
+namespace WebApplication9.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        Photo_GalleryEntities dbObj = new Photo_GalleryEntities();
+        public ActionResult Employee(Admin obj)
+        {
+            if (obj != null)
+                return View(obj);
+            else
+
+            return View();
+        }
+        [HttpPost]
+        public ActionResult AddEmployee(Admin model)
+        {
+            Admin obj = new Admin();
+            obj.ID = model.ID;
+            obj.EmployeeName = model.EmployeeName;
+            obj.Client = model.Client;
+            obj.Amount = model.Amount;
+            obj.Date = model.Date;
+            obj.PurchasedItem = model.PurchasedItem;
+            obj.Bill = model.Bill;
+
+            if(model.ID==0)
+            {
+
+         
+            dbObj.Admins.Add(obj);
+            dbObj.SaveChanges();
+            ////ModelState.Clear();
+           
+            }
+            else
+            {
+                dbObj.Entry(obj).State = System.Data.Entity.EntityState.Modified;
+                dbObj.SaveChanges();
+            }
+            return View("Employee");
+        }
+
+
+        public ActionResult EmployeeList()
+        {
+            var res = dbObj.Admins.ToList();
+
+            return View(res);
+        }
+        
+        public ActionResult Delete(int id)
+        {
+            var res = dbObj.Admins.Where(x => x.ID == id).First();
+            dbObj.Admins.Remove(res);
+            dbObj.SaveChanges();
+
+            var list = dbObj.Admins.ToList();
+
+            return View("EmployeeList",list);
+        }
+        
+     
+    }
+}
+
+    
---
+++ WebApplication9/Scripts/emp.js
@@ -0,0 +1,83 @@
+﻿function exportRecords() {
+
+    if (confirm("Do you want to Export Records ?")) {
+        locationRef('~/Employee/Export');
+    }
+}
+function locationRef(url) {
+
+    window.location.href = url;
+}
+
+
+
+
+function onload1() {
+   
+    var url = '~/Employee/EmployeeList'
+    var searchCol = [
+        { type: "text" },
+        { type: "text" },
+        { type: "text" },
+        { type: "text" },
+        { type: "text" },
+        { type: "text" },
+        { type: "text" },
+
+    ];
+
+    abc(url, searchCol);
+}
+function abc(lurl) {
+
+    $('#example').dataTable().fnDestroy();
+
+
+    $('#example').dataTable({
+
+        "aaSorting": [[0, "asc"]],
+        "sDom": 'T<"clear ">lfrtip',
+        "sPaginationType": "full_numbers",
+
+        "oTableTools": {
+
+            "aButtons": [
+
+                {
+                    "sExtends": "collection",
+                    "sButtonText": 'Save <span/>',
+                    "aButtons": [
+                        {
+                            "sExtends": "pdf",
+                            "mColumns": "visible",
+                            "oSelectorOpts": { "filter": "applied", "order": "current" },
+                            "sPdfOrientation": "landscape",
+                            "sTitle": "Student Report"
+                        },
+                        {
+                            "sExtends": "xls",
+                            "mColumns": "visible",
+                            "oSelectorOpts": { "filter": "applied", "order": "current" },
+                        },
+                        "print"
+                    ]
+                }
+            ]
+        },
+        "processing": true,
+        "ajax": {
+            "url": lurl,
+            "dataSrc": "rows",
+            "type": "GET"
+
+        }
+    });
+
+}
+
+
+
+function enableSearchButton(data) {
+    if ('select' != data.value)
+        document.getElementById("search").disabled = false;
+}
